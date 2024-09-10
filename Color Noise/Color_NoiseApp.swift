6fbdd75aa4cee4f3d9e773d1065c83615747e34c
Eric Hamiter <ehamiter@gmail.com>
2024-09-10 15:58:29 -0500

    Initial Commit

---
+++ Color Noise/Color_NoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_NoiseApp.swift
+//  Color Noise
+//
+//  Created by Eric on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Color_NoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
